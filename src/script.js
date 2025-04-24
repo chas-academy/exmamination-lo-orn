0a55744ac6eb6f9acda41dae01ff769e9351985e
lo-orn <lovisaornestal@gmail.com>
2025-04-24 10:24:25 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,41 @@
+
+const incomeArray = [];
+const expenseArray = [];
+
+const descInput = document.getElementById("desc");
+const amountInput = document.getElementById("amount");
+const incomeBtn = document.getElementById("incomeBtn");
+const expenseBtn = document.getElementById("expenseBtn");  
+const incomeList = document.getElementById("incomeList");
+const expenseList = document.getElementById("expenseList");
+const transactionList = document.getElementById("transactionList");
+const balance = document.getElementById("balance");
+
+function addTransaction() {
+    const desc = descInput.value.trim();
+    const amount = parseFloat(amountInput.value);
+    const type = this.id === "incomeBtn" ? "income" : "expense";
+
+    
+    if (type === "income") {
+        incomeArray.push({ desc, amount });
+        incomeList.innerHTML += `<li class="income">${desc} - ${amount} kr </li>`;
+    } else {
+        expenseArray.push({ desc, amount });
+        expenseList.innerHTML += `<li class="expense">${desc} - ${amount} kr </li>`;
+    }
+    descInput.value = "";
+    amountInput.value = "";
+
+    updateBalance();
+}
+
+function updateBalance() {  
+    const totalIncome = incomeArray.reduce((sum, transaction) => sum + transaction.amount, 0);
+    const totalExpense = expenseArray.reduce((sum, transaction) => sum + transaction.amount, 0);
+    const balanceAmount = totalIncome - totalExpense;
+    balance.innerHTML = `Balance: ${balanceAmount}`;
+}
+
+incomeBtn.addEventListener("click", addTransaction);
+expenseBtn.addEventListener("click", addTransaction);
